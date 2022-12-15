9ad0cefdbd9bde34afdc18fc491a0f9f18c13c7d
Filip Tomashevski <93556752+FilipTomash@users.noreply.github.com>
2022-12-15 21:52:42 +0100

    Update app.js

--- homework3-Functions/homework3-part_2/scripts/app.js
+++ homework3-Functions/homework3-part_2/scripts/app.js
@@ -1,13 +1,23 @@
-function dogAgeCalculator(yearsTheDogHas) {
+function dogAgeCalculator(convertionType, yearsInput) {
     if (
-        Number.isNaN(yearsTheDogHas) ||
-        typeof yearsTheDogHas !== "number" ||
-        yearsTheDogHas === 0
+        Number.isNaN(yearsInput) ||
+        typeof yearsInput !== "number" ||
+        yearsInput === 0
     ) {
         return "Invalid input!";
     }
-    let ageInHumanYears = yearsTheDogHas * 7;
-    return ageInHumanYears;
+    if (
+        convertionType !== "h2d" && convertionType !== "d2h"
+    ) {
+        return "Invalid input!";
+    }
+    if (convertionType === "d2h") {
+        let ageInHumanYears = yearsInput * 7;
+        return ageInHumanYears;
+    }
+
+    let ageInDogYears = yearsInput / 7;
+    return ageInDogYears;
 }
 
-console.log (dogAgeCalculator(12));
+console.log(dogAgeCalculator("d2h", 10));
